43aebe9d14c6c0f22bd83e3d07bca8da1754482f
shaikh zakir sami Sameer <122259194+Zakir-Shaikh@users.noreply.github.com>
2023-04-11 22:55:09 +0530

    Update F9 methods and methods parameters.cs

--- F9 methods and methods parameters.cs
+++ F9 methods and methods parameters.cs
@@ -29,3 +29,34 @@ class Program
     }
   }
 }
+
+//methods parameters
+/*
+there are 4 type of methods parameters
+1 value parameters
+2 reference parameters
+3 out parameters 
+4 parameters arreys
+*/
+
+class program 
+{
+  public static void main()
+  {
+    int[] numbers = new int [3];
+
+    numbers[0] = 101;
+    numbers[1] = 102;
+    numbers[2] = 103;
+
+    paramsmethod(1,2,3,4,5);
+  }
+  public static void paramsmethod (params int[]number)
+  {
+    console.writeline("there are {0} elements",numbers.lenght);
+    foreach (int i in numbers)
+    {
+      console.writeline(i);
+    }
+  }
+}
