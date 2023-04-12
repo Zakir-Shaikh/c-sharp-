b1426f9dbe2147e8126a4da35433a1121b77a3b4
shaikh zakir sami Sameer <122259194+Zakir-Shaikh@users.noreply.github.com>
2023-04-12 22:08:17 +0530

    Add files via upload

---
+++ F10 namespaces.cs
@@ -0,0 +1,45 @@
+// namespace
+/*
+namespace are used to organised your program
+it to provide assistence to avoid classhes 
+*/
+
+using System ;
+namespace projectA teamA
+{
+  class ClassA 
+  {
+    public static void print()
+    {
+      console.writeline("this is team a print method");
+    }
+
+  }   
+}
+namespace projectA TeamB
+{
+  class ClassA 
+  {
+    public static void print()
+    {
+      console.writeline("this is team a print method");
+    }
+
+  }
+}
+
+/* diffrent namespes member
+1 class
+2 another namespace
+3 interface
+4 enum
+5 struct
+6 delegate
+*/
+
+//class
+/*
+if you want to create a complex custmos types ,then we can make use of class
+a class consist of data and behaviour class data is represent by its filed and behaviour is represented by its method 
+*/
+
