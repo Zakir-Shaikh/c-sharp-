f182fb7f1022f01f91407f36545fefa6ee564e7a
shaikh zakir sami Sameer <122259194+Zakir-Shaikh@users.noreply.github.com>
2023-04-19 23:02:06 +0530

    Update F11.cs

--- F11.cs
+++ F11.cs
@@ -6,4 +6,19 @@ public class employee
   string firstname;
   string lastname;
   string email;
+  public void fullname()
+  {
+
+  }
+}
+public class full employee : employee 
+{
+  float yearlysalay;
+}
+public class program 
+{
+  public static void main()
+  {
+     float yearlysalay;
+  }
 }
